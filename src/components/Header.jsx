58fa1524cd035701f2e736d63be126142d22be13
Hassan12271 <hassandanyal18@gmail.com>
2025-08-30 17:23:25 +0500

    logo dymaic done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <header className="bg-white py-1">
       <div className="container mx-auto">
-        <img src="./src/assets/logo.png" alt="Logo" className="m-auto h-24" />
+        <img src={Logo} alt="Logo" className="m-auto h-24" />
       </div>
     </header>
   );
